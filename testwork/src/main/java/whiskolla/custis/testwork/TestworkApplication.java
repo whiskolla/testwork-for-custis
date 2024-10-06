27f5aa7b71bdbf9fce9b7c49d400da7618164010
Anastasia <bfoyb19@gmail.com>
2024-10-06 15:50:06 +0300

    add : available time zone and date

--- testwork/src/main/java/whiskolla/custis/testwork/TestworkApplication.java
+++ testwork/src/main/java/whiskolla/custis/testwork/TestworkApplication.java
@@ -4,17 +4,20 @@ import org.slf4j.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.*;
 
 @SpringBootApplication
 public class TestworkApplication {
     static Logger logger = LoggerFactory.getLogger(TestworkApplication.class);
-    private static final LocalTime startTime = LocalTime.of(9, 0);
-    private static final LocalTime endTime = LocalTime.of(15, 0);
+    private static final LocalDateTime startTime = LocalDateTime.of(2024,10,06,10,00);
+    private static final LocalDateTime endTime = LocalDateTime.of(2024,10,06,17,00);
     public static void main(String[] args) {
-        LocalTime currentTime = LocalTime.now();
+        LocalDateTime currentTime = LocalDateTime.now(ZoneId.of("Europe/Moscow"));
         if (currentTime.isBefore(startTime) || currentTime.isAfter(endTime)) {
-            logger.info("Ошибка. Окно - " + startTime + " - " + endTime + ". Сейчас - " + currentTime);
+            logger.info("Ошибка. Окно: [" + startTime + " - " + endTime + "]. Сейчас - " + currentTime);
         } else {
             SpringApplication.run(TestworkApplication.class, args);
         }
